430cd34b1b908346bd879dbbbc66aaa0d046ed13
Diego Albitres <diego.albitres@gmail.com>
2021-11-03 16:30:21 -0500

    Added argument for output directory

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import fs from 'fs/promises';
+import path from 'path';
 import consola from 'consola';
+import dedent from 'ts-dedent';
 import { fetchXML } from './utils/fetcher';
 import { parseXML } from './utils/parser';
 import { createDatabase, createDocument } from './utils/transform';
@@ -12,13 +14,36 @@ if (typeof modelId !== "string") {
   throw new Error("An invalid model ID was received.");
 }
 
+const parseArgs = (def: string): string => {
+  const args = process.argv.slice(2);
+  if (args.length === 0) {
+    return path.resolve(def);
+  }
+  return path.resolve(args[0]);
+};
+
 const run = async () => {
-  const xml = await fetchXML(modelId);
-  const parsed = parseXML(xml);
-  const database = createDatabase(parsed);
-  const document = createDocument(database);
-  await fs.mkdir("out/", { recursive: true });
-  await fs.writeFile("out/detailed-tables.tex", document, { encoding: 'utf-8' });
+  try {
+    const xml = await fetchXML(modelId);
+    const parsed = parseXML(xml);
+    const database = createDatabase(parsed);
+    const document = createDocument(database);
+    const outputDir = parseArgs('out/');
+    await fs.mkdir(outputDir, { recursive: true });
+    await fs.writeFile(
+      path.join(outputDir, 'detailed-tables.tex'),
+      document,
+      { encoding: 'utf-8' }
+    );
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.error(dedent`
+      An unknown error has ocurred: ${err}`
+    );
+  }
 };
 
 run();
