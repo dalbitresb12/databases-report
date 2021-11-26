b40f4c2f9e7cabafdf6929b5abd9aab5e8b66dee
Angel Meneses <angelmenesest123@gmail.com>
2021-11-26 15:40:04 -0500

    Fixed problems

--- sql/angel-meneses.sql
+++ sql/angel-meneses.sql
@@ -5,7 +5,6 @@ go
 begin transaction;
 go
 
-
 -- 1. Añadir una nueva columna 'Edad' en la tabla Student. 
 -- Luego, llenar esa columna con las edades de los estudiantes cada vez que ellos registren su fecha de nacimiento en su información.
 -- Para aquellos que ya tienen registrada la fecha, actualizar el campo automáticamente
@@ -16,7 +15,9 @@ go
 CREATE TRIGGER TX_SettingAge ON Student
 FOR UPDATE
 AS
-UPDATE Student SET Age = DATEDIFF(YEAR, birthday, CURRENT_TIMESTAMP);
+UPDATE Student 
+SET Age = DATEDIFF(YEAR, birthday, CURRENT_TIMESTAMP)
+WHERE birthday IS NOT NULL AND Age IS NULL
 go
 
 UPDATE Student SET birthday = '2003-01-18' WHERE student_id = 3;
@@ -24,28 +25,29 @@ UPDATE Student SET birthday = '2003-05-11' WHERE student_id = 5;
 UPDATE Student SET birthday = '2005-12-03' WHERE student_id = 7; 
 go
 
+
 -- 2. Coursera quiere saber quién fue el estudiante más joven que completó más cursos en un periodo de tiempo (entre 2 fechas)
 -- (función + subqueries)
 
-CREATE FUNCTION EstudianteCursosTerminados(@start DATE, @end DATE) RETURNS TABLE
+CREATE FUNCTION BestYoungestStudent(@start DATE, @end DATE) RETURNS TABLE
 AS
 RETURN(SELECT S.fullname as "StudentName", S.Age , COUNT(M.course_id) AS "FinishedCourses"
 FROM Membership M INNER JOIN Student S ON  M.student_id = S.student_id
 WHERE M.finished_course = 1 AND M.enrollment_date BETWEEN @start AND @end AND S.birthday IS NOT NULL
-GROUP BY S.fullname, DATEDIFF(DAY, birthday, CURRENT_TIMESTAMP)
+GROUP BY S.fullname, S.Age
 HAVING COUNT(M.course_id) >= (SELECT MAX(ECT.FinishedCourses)
 FROM (SELECT S.fullname as "StudentName", COUNT(M.course_id) AS "FinishedCourses"
 FROM Membership M INNER JOIN Student S ON  M.student_id = S.student_id
 WHERE M.finished_course = 1 AND M.enrollment_date BETWEEN @start AND @end AND S.birthday IS NOT NULL
 GROUP BY S.fullname) ECT)
-AND DATEDIFF(DAY, birthday, CURRENT_TIMESTAMP) <= (SELECT MIN(ECT.AgeDays)
-FROM (SELECT S.fullname as "StudentName", DATEDIFF(DAY, birthday, CURRENT_TIMESTAMP) AS "AgeDays"
+AND S.Age <= (SELECT MIN(ECT.Age)
+FROM (SELECT S.fullname as "StudentName", S.Age
 FROM Membership M INNER JOIN Student S ON  M.student_id = S.student_id
 WHERE M.finished_course = 1 AND M.enrollment_date BETWEEN @start AND @end AND S.birthday IS NOT NULL) ECT));
 go
 
 SELECT E.StudentName, E.FinishedCourses, E.Age 
-FROM EstudianteCursosTerminados('2021-01-01', '2021-11-26') E;
+FROM BestYoungestStudent('2021-01-01', '2021-11-26') E;
 go
 
 -- 3. Coursera quiere conocer qué institución ofrece los mejores contenido a los estudiantes. 
@@ -53,18 +55,18 @@ go
 -- (procedimiento)
 
 
-CREATE PROCEDURE BestInstitucion
+CREATE PROCEDURE BestInstitution
 AS 
-SELECT I.name AS "Institucion", ROUND(AVG(C.content_rating),2) AS "MejorRating"
+SELECT I.name AS "Institucion", ROUND(AVG(C.content_rating),2) AS "BestRating"
 FROM Course C INNER JOIN Institution I ON C.institution_id = I.institution_id
 GROUP BY I.name
-HAVING AVG(C.content_rating) >= (SELECT MAX(R.RatingPromedio)
-FROM (SELECT I.name, AVG(C.content_rating) AS "RatingPromedio"
+HAVING AVG(C.content_rating) >= (SELECT MAX(R.avgRating)
+FROM (SELECT I.name, AVG(C.content_rating) AS "avgRating"
 FROM Course C INNER JOIN Institution I ON C.institution_id = I.institution_id
 GROUP BY I.name) R);
 go
 
-EXEC BestInstitucion;
+EXEC BestInstitution;
 go
 
 -- 4. Coursera desea saber qué cursos pertenecen a la categoría más difícil entre las existentes (la que tiene el promedio más bajo de notas aprobatorias)
@@ -72,7 +74,7 @@ go
 -- (subqueries)
 
 UPDATE Course 
-SET course_type = 7
+SET course_type = 10
 WHERE course_id = (SELECT C.category_id
 FROM Course C INNER JOIN (SELECT Ca.category_id AS "CategoryID", AVG(Ce.grade_achieved) AS "AverageGrade"
 FROM Certificate Ce INNER JOIN Membership M ON Ce.certificate_id = M.certificate_code
@@ -89,9 +91,8 @@ go
 
 -- Cleanup
 DROP TRIGGER TX_SettingAge;
-DROP FUNCTION EstudianteCursosTerminados;
-DROP PROCEDURE BestInstitucion;
-ALTER TABLE Student DROP COLUMN Age;
+DROP FUNCTION BestYoungestStudent;
+DROP PROCEDURE BestInstitution;
 go
 
 rollback;
